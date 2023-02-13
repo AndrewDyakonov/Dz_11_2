88496a557b5e6848360aae2e0694cfea6dbf3c00
blendi <dronramone@mail.ru>
2023-02-14 01:29:03 +0600

    Add new funktion in str_func and docstring first funktion

--- str_func.py
+++ str_func.py
@@ -1,4 +1,13 @@
 def upper_text(text: str):
-    """Перевести текст в верхний регистр"""
+    """
+    Привести текст в верхний регистр
+    Это сделал я
+    """
     new_text = text.upper()
+    print(new_text)
+
+
+def upper_first_letter(text: str):
+    """Привести все первые буквы слова к верхнему регистру"""
+    new_text = text.title()
     print(new_text)
